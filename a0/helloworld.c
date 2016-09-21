ed07563739a2516ffcd52c5cd569d3babe3bddb3
Noah Boulton <noah.boulton@gmail.com>
2016-09-21 15:15:55 -0700

    Updated the helloworld files.

--- a0/helloworld.c
+++ a0/helloworld.c
@@ -3,7 +3,7 @@
 //This is the entry point for the program
 int main() {
     // Make a call to a function defined in hello.c 
-    hello("world");
+    hello("foo");
 
     //Exit program    
     return(0);
