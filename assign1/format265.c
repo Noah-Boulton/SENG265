a8bee7541c66082c98cb594363eea5532de4f0cc
Noah Boulton <noah.boulton@gmail.com>
2016-10-01 13:21:40 -0700

    Updated format265.c, added new functions to read input, tokenize the input line, detect formatting, and format the input line if needed.

--- assign1/format265.c
+++ assign1/format265.c
@@ -9,41 +9,67 @@ int width = 0;
 int left = 0;
 int formatting = 0;
 int spacing = 0;
+char output[MAX_INPUT_LINES * MAX_CHARS_PER_LINE];
 
-int main() {
-/* read the input file
- * tokenize the input 
- * check for the formatting keys
- * set flags for each flag (0 if false, 1 if not)
- * take in cooresponding int values for each key
- * call the formatting method passing the cooresponding paramaters
- * Maximum line will have 132 chars
- * Maximum file length is 500 lines
- */
-	return 0;
+int main(int argc, char *argv[]) {
+    FILE *data;
+    data = fopen(argv[1], "r");
+    if(data == 0){
+        return 0;
+    }
+    read_input(data);
+    return 1;
 }
 
-int formatting_type (char* current_line) {
-	char line_copy[MAX_CHARS_PER_LINE];
-	strncpy(line_copy, current_line, MAX_CHARS_PER_LINE);
-}
-int tokenizeLine() {
-	char c;
-        char buff[132];
-        c = getchar();
-        count = 0;
-        while(c != \n) {
-                buff[count] = c;
-                count++;
-        }
-	int line_width;
-	if(strcmp(buff[0],.LW) == 0){
-		/* set the line width flag */
-		line_width = buff[1];
+void read_input(FILE *data){
+	char line[132];
+	while(fgets(line, MAX_CHARS_PER_LINE, (FILE*)data)) {
+		char processed[132];
+		strncpy(line, processed, 132);
+		formating_type(processed);
+		if(formatting) {
+			format(*processed);
+		} else{
+			printf("%s",line);
+			break;
+		}
+		prinft("%s",processed);
 	}
-	
-	
 }
-int format() {
-	
+void formatting_type (char* current_line) {
+	if(strcmp(current_line[0],".LW") != 0){
+		width = current_line[1];
+		formatting = 1;
+	}else if(strcmp(current_line[0],".LM") != 0){
+		left = current_line[1];
+		formatting = 1;
+	} else if(strcmp(current_line[0],".LS") != 0){
+		spacing = current_line[1];
+		formatting = 1;
+	}
+    
 }
+void tokenize(char *processed) {
+	char *tokenized_output;
+	tokenized_output = strtok(processed, " ");
+
+ 	while (tokenized_output && num_words < MAX_INPUT_LINES) {
+ 		strncpy (words[num_words], tokenized_output, MAX_INPUT_LINES);
+ 		num_words++;
+ 		tokenized_output = strtok (NULL, " ");
+ 	} 
+ 	
+}
+void format(char *processed) {
+	tokenized(processed);
+	if(left) {
+		int i;
+		for( i = 0; i < left; i++){
+			/* add " " to output */
+		}
+	}
+	if(width) {
+		
+	}
+}
+
