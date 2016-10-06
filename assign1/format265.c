d1b5ffa92e6e9b22136473ca445aef0c48b9026d
Noah Boulton <noah.boulton@gmail.com>
2016-10-05 21:53:05 -0700

    Added the format method. Cleaned up the tokenizing method with the adding of the chomp function. Looks to be almost done just need to add some more comments and fix some issues with files ending in more then one new line.

--- assign1/format265.c
+++ assign1/format265.c
@@ -10,9 +10,11 @@ int width = 0;
 int left = 0;
 int formatting = 0;
 int spacing = 0;
+int newline = 1;
 char output[MAX_INPUT_LINES * MAX_CHARS_PER_LINE * 2];
 char formatted[MAX_CHARS_PER_LINE];
 int line_length = 0;
+
 /*Prototype definitions*/
 int formatting_type(char* processed);
 void format(char *processed);
@@ -22,6 +24,7 @@ int main(int argc, char *argv[]) {
 	FILE *data = fopen(argv[1], "r");
 	if(data == 0){
 		fprintf(stderr,"Unable to open %s\n", argv[1]);
+		exit(0);
 	}
 	read_input(data);
 	fclose(data);
@@ -30,7 +33,20 @@ int main(int argc, char *argv[]) {
 
 void read_input(FILE *data){
 	char line[MAX_CHARS_PER_LINE];
+	char original[MAX_CHARS_PER_LINE];
 	while(fgets(line, MAX_CHARS_PER_LINE, (FILE*)data)) {
+		strncpy(original, line, MAX_CHARS_PER_LINE);
+		chomp(line);
+		if(strlen(line) == 0){
+			strncat(output, "\n\n", MAX_CHARS_PER_LINE);
+			if(spacing) {
+				int i  = 0;
+				while(i <= spacing){
+					strncat(formatted, "\n", 1);
+				}
+			}
+			line_length = 0;
+		}
 		if(formatting_type(line)){
 			continue;
 		}
@@ -40,23 +56,16 @@ void read_input(FILE *data){
 				line_length++;
 			}
 		}
-		if(formatting && !strncmp(line, "\n", 1)){
-			strncat(output, "\n\n", MAX_CHARS_PER_LINE);
-			if(spacing){
-				int i = 0;
-				while(i < spacing){
-					strncat(formatted, "\n", 1);
-				}
-			}
-			line_length = 0;
-		}
 		if(formatting){
 			format(line);
 			strncat(output, formatted, MAX_CHARS_PER_LINE);
 			strncpy(formatted, "", MAX_CHARS_PER_LINE);
 		} else{
-			strncat(output, formatted, MAX_CHARS_PER_LINE);
+			strncat(output, original, MAX_CHARS_PER_LINE);
 		}
+	}
+	if(newline && formatting){
+		strncat(output, "\n", 1);
 	}
 	printf("%s", output);
 	return;
@@ -91,5 +100,44 @@ int formatting_type (char* processed) {
 }
 
 void format(char *processed){
-	
+	char *current_word = strtok(processed, " ");
+	while(current_word != NULL) {
+		if(line_length + strlen(current_word) >= width) {
+			strncat(formatted, "\n", 1);
+			if(spacing) {
+				int i = 0;
+				while(i < spacing){
+					strncat(formatted, "\n", 1);
+					i++;
+				}
+			}
+			line_length = 0;
+			if(left) {
+				while(line_length < left){
+					strncat(formatted, " ", 1);
+					line_length++;
+				}
+			}	
+		}
+		else if(line_length != 0 && line_length <= width+1) {
+			strncat(formatted, " ", 1);
+			line_length++;
+		}
+		line_length += strlen(current_word);
+		strncat(formatted, current_word, MAX_CHARS_PER_LINE);
+    	current_word = strtok(NULL, " ");
+	}
+	return;
+}
+
+void chomp(char *line) {
+    if(line != NULL){
+		int length = strlen(line);
+		if (line[length-1] == '\n') {
+        line[length-1] = '\0';
+    	} else {
+    		newline = 0;
+    	}
+    }
+}	
 }
