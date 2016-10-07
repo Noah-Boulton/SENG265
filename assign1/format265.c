74b17223c79d05dfbdb3aec860f51653693ab732
Noah Boulton <noah.boulton@gmail.com>
2016-10-06 22:51:51 -0700

    Fixed the newline formatting issue and the seg fault error. The patch for the newline is very ugly and should be updated. Added a bash script for testing.

--- assign1/format265.c
+++ assign1/format265.c
@@ -10,6 +10,7 @@ int width = 0;
 int left = 0;
 int formatting = 0;
 int spacing = 0;
+int newline = 1;
 char output[MAX_INPUT_LINES * MAX_CHARS_PER_LINE * 2];
 char formatted[MAX_CHARS_PER_LINE];
 int line_length = 0;
@@ -20,7 +21,7 @@ void format(char *processed);
 void read_input(FILE *data);
 void chomp(char *line);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
 	FILE *data = fopen(argv[1], "r");
 	if(data == 0){
 		fprintf(stderr,"Unable to open %s\n", argv[1]);
@@ -36,15 +37,14 @@ void read_input(FILE *data){
 	char original[MAX_CHARS_PER_LINE];
 	while(fgets(line, MAX_CHARS_PER_LINE, (FILE*)data)) {
 		strncpy(original, line, MAX_CHARS_PER_LINE);
-		if(line != NULL){
-			chomp(line);
-		}
+		chomp(line);
 		if(strlen(line) == 0){
 			strncat(output, "\n\n", MAX_CHARS_PER_LINE);
 			if(spacing) {
 				int i  = 0;
 				while(i <= spacing){
 					strncat(formatted, "\n", 1);
+					i++;
 				}
 			}
 			line_length = 0;
@@ -52,28 +52,20 @@ void read_input(FILE *data){
 		if(formatting_type(line)){
 			continue;
 		}
-		if(left){
-			while(line_length < left-1){
-				strncat(formatted, " ", 1);
-				line_length++;
-			}
-		}
 		if(formatting){
 			format(line);
 			strncat(output, formatted, MAX_CHARS_PER_LINE); 
 			strncpy(formatted, "", MAX_CHARS_PER_LINE);
 		} else{
 			strncat(output, original, MAX_CHARS_PER_LINE);
-			strncpy(original, "", MAX_CHARS_PER_LINE);
 		}
 	}
-	if(formatting){
-		if(original != NULL){
-			int length = strlen(original);
-			if(line[length-1] == '\n'){
-				strncat(output, "\n", 1);
-			}
-		}
+	if(newline == 1 && formatting == 1){
+		strncat(output, "\n\n", 1);
+	} 
+	int out_length = strlen(output);
+	if(output[out_length-1] == '\n' && output[out_length-2] == '\n' && output[out_length-3] == '\n') {
+		output[out_length-1] = '\0';
 	}
 	printf("%s", output);
 	return;
@@ -82,9 +74,9 @@ void read_input(FILE *data){
 int formatting_type (char* processed) {
 	char current_line[MAX_CHARS_PER_LINE];
 	strncpy(current_line, processed, MAX_CHARS_PER_LINE);
-	strtok(current_line, " \n");
+	strtok(current_line, " ");
 	if(!strncmp(current_line,".FT", MAX_CHARS_PER_LINE)){
-		char* fmt = strtok(current_line, " \n");
+		char* fmt = strtok(current_line, " ");
 		if(!strncmp(fmt, "on", 3)){
 			formatting = 1;
 		}else{
@@ -110,6 +102,12 @@ int formatting_type (char* processed) {
 void format(char *processed){
 	char *current_word = strtok(processed, " ");
 	while(current_word != NULL) {
+		if(left){
+			while(line_length < left-1){
+				strncat(formatted, " ", 1);
+				line_length++;
+			}
+		}
 		if(line_length + strlen(current_word) >= width) {
 			strncat(formatted, "\n", 1);
 			if(spacing) {
@@ -141,9 +139,14 @@ void format(char *processed){
 void chomp(char *line) {
 	if(line != NULL){
 		int length = strlen(line);
-		if (line[length-1] == '\n') {
-   	     	line[length-1] = '\0';
-    		}
+		if(length > 0){
+			if (line[length-1] == '\n') {
+	   	     	line[length-1] = '\0';
+	    		}
+			newline = 1;
+		}else{
+			newline = 0;
+		}	
 	}
 }	
 
